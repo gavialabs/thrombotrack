97d57b052caec11dcf40f713c3540fee9a6265e3
Anthony Y. Lee <9303869+anthonyylee@users.noreply.github.com>
2026-06-25 13:20:07 -0700

    Add CF URL to CORS origin

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -36,6 +36,7 @@ def create_app():
         origins=[
             "http://localhost:8081",
             # "https://95bf-24-22-134-158.ngrok-free.app"
+            f"https://{os.environ["CLOUDFRONT_DOMAIN_NAME"]}"
         ],
     )
 
