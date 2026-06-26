ea9ae215ac5bd85479a4ea4232b789953aa23158
Anthony Y. Lee <9303869+anthonyylee@users.noreply.github.com>
2026-06-25 23:37:31 -0700

    Test entry script

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -31,7 +31,8 @@ echo -e "${YELLOW}⏳ Waiting for database to be ready...${NC}"
 MAX_TRIES=30
 TRIES=0
 
-until psql "$DATABASE_URL" -c '\l' > /dev/null 2>&1; do
+# until psql "$DATABASE_URL" -c '\l' > /dev/null 2>&1; do
+until psql "$DATABASE_URL" -c '\l'; do
   TRIES=$((TRIES+1))
   if [ $TRIES -eq $MAX_TRIES ]; then
     echo -e "${RED}✗ Timeout: unable to connect to database${NC}"
